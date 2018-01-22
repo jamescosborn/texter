b5c763fd5bdc35d1924fc05da8785a32b7f2270f
Guest User <Guest@Epicodus-1C.local>
2018-01-22 10:56:09 -0800

    add MVC for Texter app

---
+++ Texter/Models/EnvironmentVariables.cs
@@ -0,0 +1,8 @@
+﻿namespace Texter.Models
+{
+    public static class EnvironmentVariables
+    {
+        public static string AccountSid = "AC83afb7a1e4f2ee01be73562db78d8fae";
+        public static string AuthToken = "5590f3b4e599dd15941cce15ce0afcf8";
+    }
+}
